270a04da05154a26dafa9cad78979bb2cdb71401
serj52ru <serj52ru@gmail.com>
2021-12-25 17:49:03 +0300

    25.12.2021

--- exercises/09_functions/task_9_2a.py
+++ exercises/09_functions/task_9_2a.py
@@ -47,3 +47,35 @@ trunk_config = {
     "FastEthernet0/2": [11, 30],
     "FastEthernet0/4": [17],
 }
+
+from pprint import pprint
+def generate_trunk_config(intf_vlan_mapping, trunk_template):
+    trunk_cfg = []
+    key = []
+    value = []
+
+    for intf, vlan in intf_vlan_mapping.items():
+        trunk_cfg.append(f'interface {intf}')
+        for line in trunk_template:
+            if line.endswith('allowed vlan'):
+                vlan = "".join(str(vlan)).replace("[", "").replace("]", "").replace(" ", "")
+                trunk_cfg.append(f'{line} {vlan}')
+            else:
+                trunk_cfg.append(line)
+
+    for line in trunk_cfg:
+        if 'interface' in line:
+            line = line.split()
+            key.append(line[1])
+        else:
+            value.append(line)
+
+    value = [value[i:i + 3] for i in range(0, len(value), 3)]
+
+    dict_trunk_cfg = dict(zip(key, value))
+
+
+    return dict_trunk_cfg
+
+
+pprint(generate_trunk_config(trunk_config, trunk_mode_template))
