f35f3ae726ceec3e586570d7e04dc092ae0a625c
serj52ru <serj52ru@gmail.com>
2022-02-14 22:38:28 +0300

    14.02.2022

--- exercises/12_useful_modules/answer_task_12_2.py
+++ exercises/12_useful_modules/answer_task_12_2.py
@@ -34,34 +34,20 @@
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
-ip_addresses = ['8.8.4.4', '1.1.1.1-3', '172.21.41.128-172.21.41.132']
-list_of_ips_and_ranges = ["10.1.1.1", "10.4.10.10-13", "192.168.1.12-192.168.1.15"]
 import ipaddress
 
-def convert_ranges_to_ip_list(list_of_ip):
+
+def convert_ranges_to_ip_list(ip_addresses):
     ip_list = []
-    for ip in list_of_ip:
-        try:
-            ipaddress.ip_network(ip)
-            #print(ipaddress.IPv4Address(ip))
-            ip_list.append(format(ipaddress.IPv4Address(ip)))
-        except ValueError:
-            network = ip.split("-")
-            if network[-1].isdigit():
-                new_network = network[0].split(".")
-                new_network[-1] = network[-1]
-                start_ip = ipaddress.IPv4Address(network[0])
-                end_ip = ipaddress.IPv4Address(".".join(new_network))
-                print(ipaddress.IPv4Address(".".join(new_network)))
-                for ip in range(int(start_ip), int(end_ip) + 1):
-                    ip_list.append(format(ipaddress.IPv4Address(ip)))
-            else:
-                start_ip = ipaddress.IPv4Address(network[0])
-                end_ip = ipaddress.IPv4Address(network[-1])
-                for ip in range(int(start_ip), int(end_ip) + 1):
-                    ip_list.append(format(ipaddress.IPv4Address(ip)))
-
-
+    for ip_address in ip_addresses:
+        if "-" in ip_address:
+            start_ip, stop_ip = ip_address.split("-")
+            if "." not in stop_ip:
+                stop_ip = ".".join(start_ip.split(".")[:-1] + [stop_ip])
+            start_ip = ipaddress.ip_address(start_ip)
+            stop_ip = ipaddress.ip_address(stop_ip)
+            for ip in range(int(start_ip), int(stop_ip) + 1):
+                ip_list.append(str(ipaddress.ip_address(ip)))
+        else:
+            ip_list.append(str(ip_address))
     return ip_list
-if __name__ == "__main__":
-    print(convert_ranges_to_ip_list(ip_addresses))
