f6d10ebdde2265099dcd7016d185831437bd36b9
serj52ru <serj52ru@gmail.com>
2022-03-15 22:31:30 +0300

    15.03.2022

--- exercises/15_module_re/answer_task_15_5.py
+++ exercises/15_module_re/answer_task_15_5.py
@@ -26,26 +26,22 @@ description Connected to SW1 port Eth 0/1
 
 Проверить работу функции на файле sh_cdp_n_sw1.txt.
 """
-
 import re
-from pprint import pprint
 
-def generate_description_from_cdp(file):
 
-    dict_cdp = {}
-    regex = r"(\S+) +(\S+ \S+) +\d+ +\S \S \S +\d+ +(\S+ \S+)"
+def generate_description_from_cdp(sh_cdp_filename):
+    regex = re.compile(
+        r"(?P<r_dev>\w+)  +(?P<l_intf>\S+ \S+)"
+        r"  +\d+  +[\w ]+  +\S+ +(?P<r_intf>\S+ \S+)"
+    )
+    description = "description Connected to {} port {}"
+    intf_desc_map = {}
+    with open(sh_cdp_filename) as f:
+        for match in regex.finditer(f.read()):
+            r_dev, l_intf, r_intf = match.group("r_dev", "l_intf", "r_intf")
+            intf_desc_map[l_intf] = description.format(r_dev, r_intf)
+    return intf_desc_map
 
-    with open(file, "r")as f:
-        for line in f:
-            match = re.search(regex, line)
-            if match:
-                dev_id = match.group(1)
-                local_intf = match.group(2)
-                port_id = match.group(3)
-                dict_cdp[local_intf] = f"description Connected to {dev_id} port {port_id}"
-
-    return dict_cdp
 
 if __name__ == "__main__":
     print(generate_description_from_cdp("sh_cdp_n_sw1.txt"))
-
