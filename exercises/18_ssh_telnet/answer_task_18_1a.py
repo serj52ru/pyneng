b4e5e76d023475ba59a8dbcb2e145e9e7e2d8124
serj52ru <serj52ru@gmail.com>
2022-04-19 16:31:57 +0300

    19.04.2022

--- exercises/18_ssh_telnet/answer_task_18_1a.py
+++ exercises/18_ssh_telnet/answer_task_18_1a.py
@@ -11,18 +11,19 @@
 
 Для проверки измените пароль на устройстве или в файле devices.yaml.
 """
-
 import yaml
-import netmiko
+import sys
+from netmiko import ConnectHandler
+from paramiko.ssh_exception import AuthenticationException
+
 
 def send_show_command(device, command):
-    #print(device)
     try:
-        ssh = netmiko.ConnectHandler(**device)
-        ssh.enable()
-        result = ssh.send_command(command)
-        return result
-    except netmiko.ssh_exception.NetmikoAuthenticationException as error:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+            result = ssh.send_command(command)
+            return result
+    except AuthenticationException as error:
         print(error)
 
 
@@ -30,6 +31,6 @@ if __name__ == "__main__":
     command = "sh ip int br"
     with open("devices.yaml") as f:
         devices = yaml.safe_load(f)
-
     for dev in devices:
-        print(send_show_command(dev, command))
+        result = send_show_command(dev, command)
+        print(result)
--- exercises/18_ssh_telnet/answer_task_18_1b.py
+++ exercises/18_ssh_telnet/answer_task_18_1b.py
@@ -12,26 +12,26 @@
 
 Для проверки измените IP-адрес на устройстве или в файле devices.yaml.
 """
-
 import yaml
-import netmiko
+import sys
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import SSHException
+
 
 def send_show_command(device, command):
-    #print(device)
     try:
-        ssh = netmiko.ConnectHandler(**device)
-        ssh.enable()
-        result = ssh.send_command(command)
-        return result
-    except netmiko.ssh_exception.NetmikoAuthenticationException as error:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+            result = ssh.send_command(command)
+            return result
+    except SSHException as error:
         print(error)
-    except netmiko.ssh_exception.NetmikoTimeoutException as error:
-        print(error)
+
 
 if __name__ == "__main__":
     command = "sh ip int br"
     with open("devices.yaml") as f:
         devices = yaml.safe_load(f)
-
-    for dev in devices:
-        print(send_show_command(dev, command))
+    r1 = devices[0]
+    result = send_show_command(r1, command)
+    print(result)
